3774671e5de458a156116baa4620ff50fdd5063e
Chandra Mohan Soni <chso@microsoft.com>
2021-02-01 22:32:57 -0800

    adding final differential library

--- differentialAngles.py
+++ differentialAngles.py
@@ -4,24 +4,28 @@
 import math
 
 def calcAngles(x,y,a,b):
+    x0 = math.cos(a) + math.cos(b)
+    y0 = math.sin(a) + math.sin(b)
+    dx = (x-x0)
+    dy = (y-y0)
+    ares = a
+    bres = b
+    minl = dx * dx + dy * dy
     for iteration in range(1,500) :
         x0 = math.cos(a) + math.cos(b)
         y0 = math.sin(a) + math.sin(b)
         dx = (x-x0)
         dy = (y-y0)
-        sina = math.sin(a)
-        if(sina == 0) : sina = 0.0001
-        sinb = math.sin(b)
-        if(sinb == 0) : sinb = 0.0001
-        cosa = math.cos(a)
-        if(cosa == 0) : cosa = 0.0001
-        cosb = math.cos(b)
-        if(cosb == 0) : cosb = 0.0001
-        dax = (-1.000 * (dx))/(sina)
-        dbx = (-1.000 * (dx))/(sinb)
-        day = (1.000 * dx)/(cosa)
-        dby = (1.000 * dy)/(cosb)
-        print(x0, y0, a,b, dax, day, dbx,dby)
-        a = a + 0.0001 * dax + 0.0001 * day
-        b = b + 0.0001 * dbx + 0.0001 * dby
-    return (a,b)
+        if(dx * dx + dy * dy < minl) :
+            ares = a
+            bres = b
+        sinba = math.sin(b - a)
+        if(sinba == 0) : sinba = 0.01
+        da = 1.00000 * (dy * math.sin(b) + dx * math.cos(b))/sinba
+        db = -1.00000 * (dy * math.sin(a) + dx * math.cos(a))/sinba
+        modsum = 0.0001 + abs(da) + abs(db)
+        da = da/modsum
+        db = db/modsum
+        a = a + 0.01 * da
+        b = b + 0.01 * db
+    return (ares,bres)
